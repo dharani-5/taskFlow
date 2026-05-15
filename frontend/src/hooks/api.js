460aa69a2070cef005aa385c1660268b1eaa2c8e
dharani-5 <dharanipalakurthi2003@gmail.com>
2026-05-15 11:35:43 +0530

    fix api url

--- frontend/src/hooks/api.js
+++ frontend/src/hooks/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api'
+  baseURL: 'https://taskflow-production-bb65.up.railway.app/api'
 });
 
 api.interceptors.request.use(config => {
